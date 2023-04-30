357fecdc55675cd7d94935fadbc7237024694cef
Vladyslav-Voronkov <vladyslavvoronkov@gmail.com>
2023-04-30 18:44:33 +0200

    bugs fixed

---
+++ app.js
@@ -0,0 +1,5 @@
+// Функция форматирует строку, добавляя запятые между тысячами
+function formatNumber(num) {
+    return num.toLocaleString();
+}
+// console.log(formatNumber(51230));
---
+++ app.ts
@@ -0,0 +1,7 @@
+// Функция форматирует строку, добавляя запятые между тысячами
+
+function formatNumber(num:number):string {
+  return num.toLocaleString();
+}
+
+// console.log(formatNumber(51230));
